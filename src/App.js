61017e8be5e34c6d811e8c0f3828ffcca1230e92
Maxence Dalmais <github@maxired.fr>
2023-06-22 17:39:19 +0200

    :sparkles: add streaming from a frame to another

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef } from "react";
+import logo from "./logo.svg";
 
-function App() {
+const styles = {
+  sourceIframe: {
+    width: 500,
+    height: 500,
+  },
+};
+
+function Main() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <iframe src="/#source" style={styles.sourceIframe} />
+
+      <iframe src="/#dest" style={styles.sourceIframe} />
     </div>
   );
+}
+
+function Source() {
+  const [count, setCounter] = React.useState(0);
+
+  const observerRef = useRef(null);
+  const onRef = useCallback((node) => {
+    if(observerRef.current) {
+      observerRef.current.disconnect();
+    }
+    if (node) {
+      window.parent.postMessage({ type: 'sync', data: node.innerHTML});
+
+      const config = { attributes: true, childList: true, subtree: true, characterData: true };
+      const observer = new MutationObserver(() => {debugger;
+        window.parent.postMessage({ type: 'sync', data: node.innerHTML});
+      });
+
+      observer.observe(node, config);
+      observerRef.current = observer;
+    }
+  }, []);
+  return (
+    <div ref={onRef}>
+      source
+      <button onClick={() => setCounter(count + 1)}>
+        click me for more
+      </button>
+      Current value is {count}
+      <img src={logo} />
+      <audio controls src="pop01.mp3" autoPlay/>
+    </div>
+  );
+}
+function Dest() {
+
+  const [content, setContent] = React.useState('');
+  React.useEffect(() => {
+
+    window.parent.addEventListener('message', (data) => {
+      setContent(data.data.data)
+    })
+  }, []);
+  return (
+    <div >
+      <style>{'button { visibility: hidden;}'}</style>
+      dest
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+    </div>
+  );
+}
+
+function App() {
+  if (document.location.hash === "#source") {
+    return <Source />;
+  }
+
+  if (document.location.hash === "#dest") {
+    return <Dest />;
+  }
+  return <Main />;
 }
 
 export default App;
